ff9edb254bca9bd3e527d62ad4009d9bb1da362a
Maxwell Lin <maxwell.lin@duke.edu>
2023-07-27 16:21:34 -0400

    Add snippets

---
+++ snippets/onetrust_ids.js
@@ -0,0 +1,24 @@
+// Select all elements with an ID that starts with 'ot-header-id-'
+onetrust_id_elements = document.querySelectorAll('*[id^="ot-header-id-"]');
+
+success = true;
+categories = {} // Map OneTrust ID to category name (e.g. 1: "Strictly Necessary Cookies")
+onetrust_id_elements.forEach(function (element) {
+    let onetrust_id = element.id.split('ot-header-id-')[1];  // get the id after the prefix
+    let category = element.innerText;
+
+    // Check for conflicts
+    if (onetrust_id in categories && categories[onetrust_id] !== category) {
+        let duplicate_id_elements = document.querySelectorAll(`*[id^="ot-header-id-${onetrust_id}"]`)
+        console.warn(`The same OneTrust ID maps to different categories! Conflicting elements are:`);
+        console.warn(duplicate_id_elements);
+
+        success = false;
+    }
+
+    categories[onetrust_id] = category;
+});
+
+if (success) {
+    console.log(categories)
+}
